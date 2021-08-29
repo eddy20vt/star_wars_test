7dae7ff28d6183aea289eebbfcb6378840564705
Eduardo Martinez <e.deparada@douglas.de>
2021-08-29 18:37:19 +0200

    removing mock data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,32 +10,18 @@ import CharacterDetails from './components/character-details/CharacterDetails';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import mockedPeople from './mockData/people.json';
-
 function App() {
-  const initialState = null;
-  const [characters, setCharacters] = useState(initialState);
-
-  useEffect(() => {
-    setCharacters([...mockedPeople.results]);
-  }, [])
-
 
   return (
     <Router>
       <Switch>
-        <Route path="/about">
-          {characters && <MainContainer data={[...characters].slice(0,9)}/>}
-        </Route>
         <Route path="/details/:id">
           <CharacterDetails/>
         </Route>
         <Route path="/">
           <div className="App">
             <h1>Star Wars Characters</h1>
-            {characters &&
-              <MainContainer data={[...characters].slice(0,9)}/>
-            }
+            <MainContainer/>
           </div>
         </Route>
       </Switch>
